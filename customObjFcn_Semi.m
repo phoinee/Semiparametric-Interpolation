c60fabc27a3d74706fee97f568d16ba5887d45f4
phoinee <78783042+phoinee@users.noreply.github.com>
2025-04-09 09:14:31 +0900

    Update customObjFcn_Semi.m
    incorrect basis hyperparameter optimization procedure updated

--- customObjFcn_Semi.m
+++ customObjFcn_Semi.m
@@ -1,8 +1,12 @@
-function loss = customObjFcn_Semi(params, X, Y,Sigma)
+function loss = customObjFcn_Semi(params, X, Y, Sigma, flag)
     sig_l = params(1);
     sig_f = params(2);
     K = customKernel(X, X, sig_l, sig_f) + Sigma^2*eye(length(X));
     L = chol(K, 'lower');
-    zz = Y - X/(X'/K*X)*X'/K*Y; % zz = y - Psi b
+    if flag == 1 % correct hyperparameters
+        zz = Y - X/(X'/K*X)*X'/K*Y; % zz = y - Psi b
+    else
+        zz = Y - X.^2/(X.^2'/K*X.^2)*X.^2'/K*Y; % zz = y - Psi b
+    end
     loss = 0.5 * zz'/K*zz + sum(log(diag(L)));
 end
